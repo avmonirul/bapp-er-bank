269b3c3bc90953c59ff889e8fa51ecd5916a9bce
avmonirul <monirul0ads@gmail.com>
2023-02-13 20:40:20 +0600

    first commit

--- js/login.js
+++ js/login.js
@@ -10,6 +10,14 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     //3.2 get the element
     //3.3 get the value from the element
     const passwordField = document.getElementById('user-password');
-    const password = password.value;
-    console.log(email, password);
+    const password = passwordField.value;
+
+    //DANGER DO NOT VERIFY email password on the click side 
+    // step-4: veriffy email and password click whether valid user or not 
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }
+    else{
+      alert('OUT')
+    }
 })
