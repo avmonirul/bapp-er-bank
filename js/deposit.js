bc1e1d375b12fdabf1cde90c0f32e9ca3b375b7c
avmonirul <monirul0ads@gmail.com>
2023-02-14 11:39:38 +0600

    deposit ammount

--- js/deposit.js
+++ js/deposit.js
@@ -3,10 +3,16 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     // step no- 2. get the deposit ammount from the diposit input field
     // for input field use .value to the value  inside the input field
     const depositField = document.getElementById('deposit-field');
-    const depositAmmount = depositField.value;
+    const newdepositAmmount = depositField.value;
     // step no- 3. get the deposit total ammount
     // for non input (element other than input,textarea)use inner text to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmmount;
+    const previousdepositTotal = depositTotalElement.innerText;
+
+    const currentDepositTotal = previousdepositTotal + newdepositAmmount;
+
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step no- 4. clear thre deposit field
+    depositField.value = '';
 })
