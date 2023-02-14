67e895e1eb2bb57ae0c7c215c8a791921525b157
avmonirul <monirul0ads@gmail.com>
2023-02-14 17:01:03 +0600

    deposit amount

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,3 +1,7 @@
 // add even handler with the withdraw button 
+// get the withdraw ammouny=t from the withdraw input field 
 // step-1
-document.getElementById('btn-widthraw').addEventListener('click')
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // step-2
+    const withdrawField = document.getElementById('withdraw-field')
+})
