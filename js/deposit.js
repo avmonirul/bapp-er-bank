cc2aa7202cfe408f3a39b7c30a20da26924468db
avmonirul <monirul0ads@gmail.com>
2023-02-14 14:47:06 +0600

    deposit amount

--- js/deposit.js
+++ js/deposit.js
@@ -12,10 +12,20 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const previousDepositTotalStrung = depositTotalElement.innerText;
     const previousDepositTotal = parseFloat(previousDepositTotalStrung);
     console.log(typeof previousDepositTotal);
+
+    // step no- 4.add number to set the total deposit
     const currentDepositTotal = previousDepositTotal + newDepositAmmount;
-
+    //set the deposit total
     depositTotalElement.innerText = currentDepositTotal;
+    // step no- 5.get balance current total 
+    const blanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = blanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    // step no- 6.claculate current total blance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmmount;
+    //set the blance total
+    blanceTotalElement.innerText = currentBalanceTotal;
 
-    // step no- 4. clear thre deposit field
+    // step no- 7. clear thre deposit field
     depositField.value = '';
 })
